fa7eac2f4892dc88d04f122985ab8c97290a88f5
sakura <2082320282@qq.com>
2021-09-09 09:52:31 +0800

    enter

--- js/test.js
+++ js/test.js
@@ -1,11 +1,8 @@
-function sum(arr){
-    var max=arr[0];
-    for(i=1;i<arr.length;i++){
-        if(arr[i]>max){
-            max = arr[i]
-        }
-    }
-    return max
+var a = 18;
+f1();
+function f1(){
+    var b = 9;
+    console.log(a);
+    console.log(b);
+    var a = '123';
 }
-var arr=[5,2,99,101,67,77]
-console.log(sum(arr))
